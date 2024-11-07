bbfedac98dd442734f7390c5b4c0264e3077a326
Juan Pablo Carvajal <jcarvajalabrahams@gmail.com>
2024-11-07 00:14:32 -0600

    change cache acess function

--- cache/cache.py
+++ cache/cache.py
@@ -1,3 +1,4 @@
+from collections import deque
 import math
 
 class Cache:
@@ -14,53 +15,49 @@ class Cache:
         self.B = int(self.C/self.b) # Número de bloques
         self.S = int(self.B/self.N) # Sets
 
-        self._init_SRAM()
+        self._ini_SRAM()
        
        
     def _init_SRAM(self) -> None:    
 
         block = [[None]*self.B]
         self.TAG_SRAM = {i: [None]*self.N for i in range(self.S)}
-        self.DATA_SRAM = {i: block*self.N for i in range(self.S)}
+        #self.DATA_SRAM = {i: block*self.N for i in range(self.S)}
         self.VALID = {i: [None]*self.N for i in range(self.S)}
-        self.LRU = {i: [None]*2 for i in range(self.S)}
+        self.LRU = {i: None for i in range(self.S)}
 
-    def write(self, addr:int):
-        pass
+    def _ini_SRAM(self) -> None:
+        self.SRAM = {i: deque(maxlen=self.N) for i in range(self.S)}
+        self.VALID = {i: [None]*self.N for i in range(self.S)}
 
-    def read(self, addr:int) -> tuple:
-        
+    def access(self, addr:int) -> bool:
+
         HIT = True
+        MISS = False
 
         tag_bits = self._get_tag_bits(addr)
-        set_bits = self._get_tag_bits(addr)
-        block_bits = self._get_block_bits(addr)
+        set_bits = self._get_set_bits(addr)
 
-        tag_set = self.TAG_SRAM[set_bits]
-        data_set = self.DATA_SRAM[set_bits]
-
-        if tag_bits in tag_set:
-            way_index = tag_set.index(tag_bits)
-            is_valid = self.VALID[set_bits][way_index]
-
-            if is_valid:
-                data_block = data_set[way_index]
-                data = data_block[block_bits]
-                return (HIT, data)
-            
+        cache_set = self.SRAM[set_bits]
+        if tag_bits in cache_set:
+            cache_set.remove(tag_bits)
+            cache_set.append(tag_bits) # Mueve el tag a MRU
+            return HIT
         
-        return (not HIT, None)
-    
+        if len(cache_set) == self.S:
+            cache_set.popleft()
 
-    def _update_LRU(self) -> None:
-        pass
+        cache_set.append(tag_bits)
+
+        return MISS
+
 
     def _get_tag_bits(self, addr:int) -> int:
         set_bits = math.log2(self.S)
         block_bits = math.log2(self.b)
 
-        tag_bits = self.MEMORY_ADDR_LEN - self.BYTE_OFFSET - set_bits - block_bits
-        tag_start = self.BYTE_OFFSET + set_bits + block_bits
+        tag_bits = Cache.MEMORY_ADDR_LEN - Cache.BYTE_OFFSET - set_bits - block_bits
+        tag_start = Cache.BYTE_OFFSET + set_bits + block_bits
 
         return self._bitsel(addr,tag_start,tag_bits)
 
@@ -68,21 +65,25 @@ class Cache:
         set_bits = math.log2(self.S)
         block_bits = math.log2(self.b)
 
-        set_start = self.BYTE_OFFSET + block_bits
+        set_start = Cache.BYTE_OFFSET + block_bits
 
         return self._bitsel(addr,set_start,set_bits)
 
     def _get_block_bits(self, addr:int) -> int:
         block_bits = math.log2(self.b)
-        block_start = self.BYTE_OFFSET
+        block_start = Cache.BYTE_OFFSET
 
         return self._bitsel(addr,block_start,block_bits)
         
 
     def _bitsel(self, word:int, start:int, bits:int) -> int:
-        mask = (1 << bits) - 1
-        return (word >> start) & mask
+        mask = (1 << int(bits)) - 1
+        return (int(word) >> int(start)) & mask
 
 
-cache = Cache(8, 2, 2)
-print(cache.VALID)
+cache = Cache(16, 2, 2)
+cache.access(0x30003770)
+cache.access(0x30003bf8)
+cache.access(0x30003720)
+cache.access(0x30103bf8)
+print(cache.SRAM)
